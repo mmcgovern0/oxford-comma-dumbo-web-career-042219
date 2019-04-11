d74aceb3b873c614e43e166be9c4bc1cba9f577d
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-11 18:38:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+      array.last.prepend(" and ")
+      array.join
+  elsif array. length > 2
+      array.last.prepend("and ")
+      array.join(", ")
+    end
 end
